d9e192c3540b5732a5540bc0e0f8e7539bd52d98
hrtmnj <hartm651@d.umn.edu>
2023-10-11 13:27:38 -0500

    Changed links

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Bulldog Rocketry</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.bulldogrocketry.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Current Bulldog Website
         </a>
       </header>
     </div>
